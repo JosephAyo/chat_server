a9d1c12679d4c8ba4859e833b3fa369fd90297d9
ayojoseph <josephayo110@gmail.com>
2023-01-19 04:13:05 +0100

    ✨ add accept client connection feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{
+    io::{ErrorKind, Read, Write},
+    net::TcpListener,
+    sync::mpsc::channel,
+    thread,
+    time::Duration,
+};
+
+const LOCAL_ADDR: &str = "127.0.0.1:3422";
+const MESSAGE_SIZE: usize = 32;
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind(LOCAL_ADDR).expect("listener failed to bind to local address");
+    server
+        .set_nonblocking(true)
+        .expect("server failed to set to non blocking");
+
+    let mut clients = vec![];
+
+    let (sender, receiver) = channel::<String>();
+
+    loop {
+        match server.accept() {
+            Ok((mut _socket, addr)) => {
+                println!("Client {} connected", addr);
+                let client = _socket.try_clone().expect("failed to clone socket");
+                let sender = sender.clone();
+
+                clients.push(client);
+            }
+            Err(e) => {
+                println!("couldn't get client: {e:?}");
+                break;
+            }
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    }
 }
