84b737a3eb0191e7b33e6d81fdb885e86b6e8b5f
ayojoseph <josephayo110@gmail.com>
2023-01-19 04:13:46 +0100

    ✨ add client thread to send message over channel

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,32 @@ fn main() {
                 let sender = sender.clone();
 
                 clients.push(client);
+
+                thread::spawn(move || loop {
+                    let mut message_buff = vec![0; MESSAGE_SIZE];
+                    match _socket.read_exact(&mut message_buff) {
+                        Ok(_) => {
+                            let message =
+                                String::from_utf8(message_buff).expect("buffer not valid utf8");
+                            println!("client {} sent message {}", addr, message);
+                            sender
+                                .send(message)
+                                .expect("failed to send message over channel");
+                        }
+                        Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                        Err(_) => {
+                            println!("closing {}'s connection", addr);
+                            break;
+                        }
+                    }
+                });
             }
             Err(e) => {
                 println!("couldn't get client: {e:?}");
                 break;
             }
         }
+
 
         thread::sleep(Duration::from_millis(100));
     }
